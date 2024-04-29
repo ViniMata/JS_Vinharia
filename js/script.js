0e68cc176a7492d870c116731778ed4ba4530d4a
ViniMata <vinicius.matareli2005@gmail.com>
2024-04-29 14:35:54 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("Denis")
+function verificarIdade(idadeMinima) {
+    var idade = prompt("Digite sua idade:");
+    if (idade >= idadeMinima) {
+        window.location.href = "/JS_Vinharia/html/home.html";
+    } else {
+        window.location.href = "https://encurtador.com.br/pyGN0";
+    }
+}
+
+function redirecionarMenorIdade() {
+    window.location.href = "https://encurtador.com.br/pyGN0";
+}
